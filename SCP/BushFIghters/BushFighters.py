1fe26aed0af77021ed793e363afd63be5e3ecb5a
minhle10 <lems@bc.edu>
2021-11-07 01:02:25 -0500

    Update BushFighters.py

--- SCP/BushFIghters/BushFighters.py
+++ SCP/BushFIghters/BushFighters.py
@@ -2,10 +2,15 @@ from Fighter import Fighter
 from Move import Move
 
 bush_fighters = []
-bush_fighters += Fighter("David", 1, 5, 5, 10, ["homework assist"], 10)
-bush_fighters += Fighter("Kenny", 1, 5, 5, 10, ["homework assist"], 10)
-bush_fighters += Fighter("Sammy", 1, 5, 5, 10, ["homework assist"], 10)
-bush_fighters += Fighter("Sarah", 1, 5, 5, 10, ["homework assist"], 10)
+David = Fighter("David", 1, 5, 5, 10, ["homework assist"], 10)
+bush_fighters += David
+Kenny = Fighter("Kenny", 1, 5, 5, 10, ["homework assist"], 10)
+bush_fighters += Kenny
+Sammy = Fighter("Sammy", 1, 5, 5, 10, ["homework assist"], 10)
+bush_fighters += Sammy
+Sarah = Fighter("Sarah", 1, 5, 5, 10, ["homework assist"], 10)
+bush_fighters += Sarah
 
 
 #name, level, attack, defense, health, moves, maxhealth, experience = 0
+
