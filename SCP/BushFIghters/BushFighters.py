69282492b5f9584b3d83a7db23aed9e741090e45
minhle10 <lems@bc.edu>
2021-11-06 22:57:13 -0400

    bush fighter + new moves

--- SCP/BushFIghters/BushFighters.py
+++ SCP/BushFIghters/BushFighters.py
@@ -2,4 +2,8 @@ from Fighter import Fighter
 from Move import Move
 
 bush_fighters = []
-bush_fighters += Fighter("David", )
+bush_fighters += Fighter("David", 1, 5, 5, 10, ["homework assist"], 10)
+bush_fighters += Fighter("Kenny", 1, 5, 5, 10, ["homework assist"], 10)
+
+
+#name, level, attack, defense, health, moves, maxhealth, experience = 0
