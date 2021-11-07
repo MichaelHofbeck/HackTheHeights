c3ec597ba2a9d1ea4391fcbe4d3f20b64147e7ae
MichaelHofbeck <71472310+MichaelHofbeck@users.noreply.github.com>
2021-11-07 12:32:53 -0500

    Working Build

--- SCP/BushFIghters/BushFighters.py
+++ SCP/BushFIghters/BushFighters.py
@@ -6,11 +6,14 @@ David = Fighter("David", 1, 5, 5, 10, ["homework assist"], 10, "GUI/pngs/wildDav
 Kenny = Fighter("Kenny", 1, 5, 5, 10, ["homework assist"], 10, "GUI/pngs/wildKenny.png")
 Sammy = Fighter("Sammy", 1, 5, 5, 10, ["homework assist"], 10, "GUI/pngs/wildSammy.png")
 Sarah = Fighter("Sarah", 1, 5, 5, 10, ["homework assist"], 10, "GUI/pngs/wildSarah.png")
+Carl = Fighter("Carl", 1, 5, 5, 10, ["homework assist"], 10, "GUI/pngs/carlmctague.png")
+Naomi = Fighter("Naomi", 1, 5, 5, 10, ["homework assist"], 10, "GUI/pngs/naomi.png")
 
 bush_fighters.append(David)
 bush_fighters.append(Kenny)
 bush_fighters.append(Sammy)
 bush_fighters.append(Sarah)
+
 
 
 #name, level, attack, defense, health, moves, maxhealth, experience = 0
